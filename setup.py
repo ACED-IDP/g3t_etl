ef8823ecb8b9117a92fd521a01f2aa83e8735e75
Brian Walsh <brian@bwalsh.com>
2024-05-23 15:56:17 -0700

    ensure templates included in distro

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
 
     # Versions should comply with PEP 440:
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1rc12',  # Required
+    version='0.0.1rc16',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -90,7 +90,7 @@ setup(
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
 
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'sample_transformer']),  # Required
 
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
@@ -111,7 +111,7 @@ setup(
     # installed, specify them here.
     #
     package_data={  # Optional
-        '': ['*.yaml'],
+        '': ['*.yaml', 'templates/*.jinja'],
     },
 
     # To provide executable scripts, use entry points in preference to the
